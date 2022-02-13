8ce3f19d52d6050bff3dbfcd55cf924201c1f173
atamakonkurii <morisudesu@gmail.com>
2022-02-13 19:31:01 +0900

    userImgaeCommponentを追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,11 @@
+import * as nextImage from 'next/image';
+import "tailwindcss/tailwind.css";
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => <img {...props} />
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
