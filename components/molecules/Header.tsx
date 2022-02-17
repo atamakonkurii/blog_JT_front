00508f74c18ac01b2e775d69170289658fc9a989
atamakonkurii <morisudesu@gmail.com>
2022-02-18 07:54:16 +0900

    ヘッダーコンポーネントを作成

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -1,53 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from '../atoms/Button';
-import './header.css';
+import { Button } from "../atoms/Button";
+import { UserImage } from "../atoms/UserImage";
+import "./header.css";
 
 type User = {
   name: string;
+  image: string;
 };
 
 interface HeaderProps {
   user?: User;
-  onLogin: () => void;
-  onLogout: () => void;
-  onCreateAccount: () => void;
 }
 
-export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
+export const Header = ({ user }: HeaderProps) => (
   <header>
-    <div className="wrapper">
+    <div className="flex items-center justify-between h-14 bg-nittai_teal shadow text-white">
       <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fillRule="evenodd">
-            <path
-              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-              fill="#FFF"
-            />
-            <path
-              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-              fill="#555AB9"
-            />
-            <path
-              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-              fill="#91BAF8"
-            />
-          </g>
-        </svg>
-        <h1>Acme</h1>
+        <span className="ml-2 text-lg md:text-2xl font-bold">日台one!</span>
       </div>
       <div>
         {user ? (
-          <>
-            <span className="welcome">
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
-          </>
+          <div className="flex flex-row items-center">
+            <span className="text-xs pr-4">日本語⇄繁體中文</span>
+            <div className="pr-2">
+              <UserImage size="small" userImageUrl={user.image} />
+            </div>
+          </div>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <span className="text-xs pr-4">日本語⇄繁體中文</span>
           </>
         )}
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nittai_black': '#222831',
+        'nittai_grey': '#393E46',
+        'nittai_teal_thin' : '#8fecf3',
+        'nittai_teal': '#00ADB5',
+        'nittai_teal_strong' : '#014c4e',
+        'nittai_whitegrey': '#EEEEEE',
+      }
+    },
   },
   variants: {
     extend: {},
