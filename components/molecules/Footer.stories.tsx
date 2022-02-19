2321bdcf457d14dd3e809100485e8b650da0952f
atamakonkurii <morisudesu@gmail.com>
2022-02-19 21:49:22 +0900

    footerコンポーネントを作成

--- components/molecules/Footer.stories.tsx
+++ components/molecules/Footer.stories.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Footer } from './Footer';
+import { Footer } from "./Footer";
 
 export default {
-  title: 'Example/Footer',
+  title: "Example/Footer",
   component: Footer,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 } as ComponentMeta<typeof Footer>;
 
-const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />;
+const Template: ComponentStory<typeof Footer> = (args) => <Footer />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {
-    name: 'Mimo',
-    image: 'https://static.nittai-one.com/uploads/user/avatar/2/CE1865EF-A2B8-4E83-9701-08EBF1E5E68C.jpeg',
-  },
-};
-
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+export const Normal = Template.bind({});
--- components/molecules/Footer.tsx
+++ components/molecules/Footer.tsx
@@ -1,34 +1,101 @@
 import React from "react";
 
-import { UserImage } from "../atoms/UserImage";
+export const Footer = () => (
+  <footer className="bg-nittai_black text-gray-600 body-font">
+    <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <a className="flex title-font font-medium items-center md:justify-start justify-center text-nittai_whitegrey">
+        <i className="fa fa-dog fa-lg text-gray-500"></i>
+        <span className="text-gray-500 ml-1 text-xl">日台one!</span>
+      </a>
+      <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+        © 2021 Nittaione!
+      </p>
 
-type User = {
-  name: string;
-  image?: string;
-};
-
-interface FooterProps {
-  user?: User;
-}
-
-export const Footer = ({ user }: FooterProps) => (
-  <header>
-    <div className="flex items-center justify-between h-14 bg-nittai_teal shadow text-white">
-      <span className="ml-2 text-lg md:text-2xl font-bold">日台one!</span>
-      <div>
-        {user ? (
-          <div className="flex flex-row items-center">
-            <span className="text-xs pr-4">日本語⇄繁體中文</span>
-            <div className="pr-2">
-              <UserImage size="small" userImageUrl={user.image} />
-            </div>
-          </div>
-        ) : (
-          <>
-            <span className="text-xs pr-4">日本語⇄繁體中文</span>
-          </>
-        )}
+      <div className="flex flex-col text-sm mx-auto text-center sm:my-0 my-2">
+        <a
+          href="https://nittai-one.com/articles/9"
+          className="text-gray-600 ml-1"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Terms of Service
+        </a>
+        <a
+          href="https://forms.gle/sXB22Zerp76a2v7U7"
+          className="text-gray-600 ml-1"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Contact us
+        </a>
       </div>
+
+      <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <a
+          href="https://www.facebook.com/atamakonkurii"
+          className="text-gray-500"
+        >
+          <svg
+            fill="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-5 h-5"
+            viewBox="0 0 24 24"
+          >
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        <a
+          href="https://twitter.com/atamakonkurii"
+          className="ml-3 text-gray-500"
+        >
+          <svg
+            fill="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-5 h-5"
+            viewBox="0 0 24 24"
+          >
+            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+          </svg>
+        </a>
+        <a
+          href="https://www.instagram.com/atamakonkurii"
+          className="ml-3 text-gray-500"
+        >
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-5 h-5"
+            viewBox="0 0 24 24"
+          >
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UC2JHTgLF0wHhkKIPS9sTl0w"
+          className="ml-3 text-gray-500"
+        >
+          <svg
+            fill="currentColor"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="0"
+            className="w-5 h-5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+          </svg>
+        </a>
+      </span>
     </div>
-  </header>
+  </footer>
 );
