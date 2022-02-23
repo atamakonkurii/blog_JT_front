7dfcd641f6b8d9ebeac5e6669b11baf22c8de93d
atamakonkurii <morisudesu@gmail.com>
2022-02-23 18:05:01 +0900

    記事一覧のタイトルを取得

--- components/organisms/Articles.tsx
+++ components/organisms/Articles.tsx
@@ -9,45 +9,52 @@ type User = {
   image: string;
 };
 
+type Article = {
+  attributes: {
+    title_ja: string;
+    content_ja: string;
+    title_image: { url: string };
+    created_at: string;
+  };
+};
+
 interface ArticlesProps {
-  user?: User;
+  loginUser?: User;
+  articles?: any;
 }
 
 export const Articles: React.VFC<ArticlesProps> = (props) => {
   return (
     <>
-      <Header user={props.user} />
+      <Header user={props.loginUser} />
       <div className="flex flex-wrap m-2 max-w-screen-xl mx-auto">
-        <ArticleSummary
-          user={{
-            image:
-              "https://static.nittai-one.com/uploads/user/avatar/2/CE1865EF-A2B8-4E83-9701-08EBF1E5E68C.jpeg",
-            name: "Mimo",
-          }}
-        />
-        <ArticleSummary
-          user={{
-            image:
-              "https://static.nittai-one.com/uploads/user/avatar/2/CE1865EF-A2B8-4E83-9701-08EBF1E5E68C.jpeg",
-            name: "Mimo",
-          }}
-        />
-        <ArticleSummary
-          user={{
-            image:
-              "https://static.nittai-one.com/uploads/user/avatar/2/CE1865EF-A2B8-4E83-9701-08EBF1E5E68C.jpeg",
-            name: "Mimo",
-          }}
-        />
-        <ArticleSummary
-          user={{
-            image:
-              "https://static.nittai-one.com/uploads/user/avatar/2/CE1865EF-A2B8-4E83-9701-08EBF1E5E68C.jpeg",
-            name: "Mimo",
-          }}
-        />
+        {props.articles.map((article: Article) => {
+          const title_image = article.attributes.title_image.url;
+          const title = article.attributes.title_ja;
+          const content = article.attributes.content_ja;
+          const created_at = article.attributes.created_at;
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <ArticleSummary
+              article={{
+                title_image: title_image,
+                title: title,
+                content: content,
+                created_at: created_at,
+              }}
+              user={{
+                image:
+                  "https://static.nittai-one.com/uploads/user/avatar/2/CE1865EF-A2B8-4E83-9701-08EBF1E5E68C.jpeg",
+                name: "MIMO",
+              }}
+            />
+          );
+        })}
       </div>
       <Footer />
     </>
   );
 };
+
+// image: {article.attributes.title_image.url}
+// "https://static.nittai-one.com/uploads/user/avatar/2/CE1865EF-A2B8-4E83-9701-08EBF1E5E68C.jpeg"
