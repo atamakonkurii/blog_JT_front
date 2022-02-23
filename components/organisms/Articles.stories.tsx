26d95925b7d9c7441c73d8f566ba45dc688d4121
atamakonkurii <morisudesu@gmail.com>
2022-02-23 18:20:53 +0900

    era-wo

--- components/organisms/Articles.stories.tsx
+++ components/organisms/Articles.stories.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 // import { ComponentStory, ComponentMeta } from "@storybook/react";
 // import { within, userEvent } from "@storybook/testing-library";
 // import { Articles } from "./Articles";
